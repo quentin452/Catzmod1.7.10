adb3d4e22e469240a478caea821ecea59320d987
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-03-21 12:40:42 +0100

    update

--- src/main/java/fr/iamacat/catmod/proxy/ClientProxy.java
+++ src/main/java/fr/iamacat/catmod/proxy/ClientProxy.java
@@ -4,7 +4,6 @@ import cpw.mods.fml.client.registry.RenderingRegistry;
 import fr.iamacat.catmod.entities.CatAgressiveEntity;
 import fr.iamacat.catmod.init.RenderMod;
 import fr.iamacat.catmod.models.ModelCatAgressiveEntity;
-import fr.iamacat.catmod.renders.RenderCatAgressiveEntity;
 import net.minecraft.client.Minecraft;
 
 public class ClientProxy extends CommonProxy{
