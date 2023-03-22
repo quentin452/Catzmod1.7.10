62594d9ef5fb24da502be3bc9ae79e2dce80f0f8
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-03-22 09:06:19 +0100

    Update Catmod.java

--- src/main/java/fr/iamacat/catmod/Catmod.java
+++ src/main/java/fr/iamacat/catmod/Catmod.java
@@ -1,17 +1,13 @@
 package fr.iamacat.catmod;
 
-import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import fr.iamacat.catmod.entities.tnt.EntityCatTnt;
 import fr.iamacat.catmod.init.RegisterEntity;
 import fr.iamacat.catmod.proxy.CommonProxy;
-import fr.iamacat.catmod.renders.tnt.RenderCatTnt;
 import fr.iamacat.catmod.init.RegisterBlocks;
 import fr.iamacat.catmod.init.RegisterItems;
 import fr.iamacat.catmod.utils.CatTab;
